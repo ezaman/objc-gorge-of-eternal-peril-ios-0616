cb7d7289facf8cbbdd29380d2739f4cf783ef4a5
ezaman <ehsanmzaman@gmail.com>
2016-06-21 10:12:17 -0400

    done

--- FISGorgeOfEternalPerilViewController.h
+++ FISGorgeOfEternalPerilViewController.h
@@ -4,4 +4,10 @@
 
 @interface FISGorgeOfEternalPerilViewController : UIViewController
 
+//-(void)viewDidLoad;
+
+//-(void)didReceiveMemoryWarning;
+
+@property (weak, nonatomic) IBOutlet UIButton *acceptYourFateTapped;
+
 @end
--- FISGorgeOfEternalPerilViewController.m
+++ FISGorgeOfEternalPerilViewController.m
@@ -1,7 +1,24 @@
 //  FISGorgeOfEternalPerilViewController.m
 
 #import "FISGorgeOfEternalPerilViewController.h"
+@interface FISGorgeOfEternalPerilViewController ()
+
+-(IBAction)acceptYourFateTapped:(id)sender;
+
+@end
 
 @implementation FISGorgeOfEternalPerilViewController
+
+-(void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+-(void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+- (IBAction)acceptYourFateTapped:(id)sender {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
 
 @end
